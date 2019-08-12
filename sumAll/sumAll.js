55b5bf0bdb1658bc076475c9209ef57a63b2f3c6
Alex Sutton <alexsutton14@hotmail.com>
2019-08-12 13:55:36 +0100

    Completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(inputA, inputB) {
+    let currentSum = 0;
+    let i;
+    let max;
+    if(typeof inputA != "number" || typeof inputB != "number"){
+        return "ERROR";
+    }
+    if(inputA<0 || inputB < 0){
+        return "ERROR";
+    }
+    if(inputA < inputB){
+        i = inputA;
+        max = inputB;
+    } else if (inputA > inputB){
+        i = inputB;
+        max = inputA;
+    }
+    while (i <= max){
+        currentSum += i;
+        i++;
+    }
+    return currentSum;
 }
 
 module.exports = sumAll
