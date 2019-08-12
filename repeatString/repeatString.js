cd76ab1f40139613e79c0c11075a85286c80d24a
Alex Sutton <alexsutton14@hotmail.com>
2019-08-12 13:34:26 +0100

    completed reverseString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, count) {
+    let i = 0;
+    let outputString = "";
+    if(count < 0){
+        return "ERROR";
+    }
+    while (i<count){
+        outputString += inputString;
+        i++;
+    }
+    return outputString;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    let currentIndex = inputString.length -1;
+    while(currentIndex >= 0){
+        let currentChar = inputString.charAt(currentIndex);
+        currentIndex--;
+        outputString + currentChar;
+    }
+    return outputString;
 }
-
+console.log(reverseString("input String"));
 module.exports = reverseString
