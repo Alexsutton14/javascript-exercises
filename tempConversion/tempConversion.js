69503f5744cbde8b4c67d58ff5ed2012111a4061
Alex Sutton <alexsutton14@hotmail.com>
2019-08-13 21:42:02 +0100

    Completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let output = (input - 32) * (5/9);
+  output = output.toFixed(1);
+  return parseFloat(output);
 }
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let output =  input * (9/5) + 32;
+  output = output.toFixed(1);
+  return parseFloat(output);
 }
 
 module.exports = {
