f86420eb2f945ea803ef6059ed9c0f98022490a6
Alex Sutton <alexsutton14@hotmail.com>
2019-08-20 16:38:11 +0100

    completed findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-let findTheOldest = function() {
-
+function calculateAge(person){
+    let tempDeathYear = new Date().getFullYear();
+    console.log(`Current year is ${tempDeathYear}`);
+    if("yearOfDeath" in person){
+        tempDeathYear = person.yearOfDeath;
+    }
+    let age = tempDeathYear - person.yearOfBirth;
+    return age;
 }
-
+let findTheOldest = function(people) {
+    let currentOldest;
+    let currentOldestAge = 0;
+    people.forEach(element => {
+        element.age = calculateAge(element);
+    });
+    people.forEach(element =>{
+        if(element.age > currentOldestAge){
+            currentOldestAge = element.age;
+            currentOldest = element;
+        }
+    })
+    return currentOldest;
+}
 module.exports = findTheOldest
