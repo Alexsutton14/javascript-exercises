6e7ba6c00b496c66a5ff75c87a799f5945f230d2
Alex Sutton <alexsutton14@hotmail.com>
2019-08-20 16:02:09 +0100

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(request) {
+    let previous1 = 1;
+    let previous2 = 1;
+    let currentValue = 1;
+    if(request < 1){
+        return "OOPS"
+    }
+    if (request <= 2){
+        return 1;
+    }
+    for(let i = 3; i <= request; i++){
+        currentValue = (previous1 + previous2);
+        previous1 = previous2;
+        previous2 = currentValue;
+    }
+    return currentValue;
 }
 
 module.exports = fibonacci
